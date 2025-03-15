dda860f87a98d18b06898009530f67b47a2808ff
Lakshan madhusanka <lakshan.madhusanka@ivedha.com>
2025-03-15 16:40:43 +0530

    create custom hook to fetch data Fetch data from external api

---
+++ components/SearchBar.tsx
@@ -0,0 +1,33 @@
+import { Image, StyleSheet, Text, TextInput, View } from "react-native";
+import React from "react";
+import { icons } from "@/constants/icons";
+
+interface Props {
+  onPress?: () => void;
+  placeholder: string;
+}
+
+const SearchBar = ({ onPress, placeholder }: Props) => {
+  return (
+    <View className="flex-row items-center bg-dark-100 rounded-full px-5 py-4">
+      <Image
+        source={icons.search}
+        className="size-5"
+        resizeMode="contain"
+        tintColor="#ab8bff"
+      />
+      <TextInput
+        onPress={onPress}
+        placeholder={placeholder}
+        value=""
+        onChangeText={() => {}}
+        placeholderTextColor="#a8b5db"
+        className="flex-1 text-white ml-2"
+      />
+    </View>
+  );
+};
+
+export default SearchBar;
+
+const styles = StyleSheet.create({});
